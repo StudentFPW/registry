6675f8a4d6b21b740fe96a2cd47e2a0bb508f48f
LEONID JOFE <jofeleonids00@gmail.com>
2024-11-29 23:39:11 +0200

    Update settings.py

--- registry_factory/registry_factory/settings.py
+++ registry_factory/registry_factory/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = str(os.getenv("SECRET_KEY"))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["143.110.151.16"]
 
 
 # Application definition
